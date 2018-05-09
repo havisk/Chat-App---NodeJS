cf06e311cfb2ac71bff285b6db9ca241a16209ce
Kool Havis <havisk@gmail.com>
2018-05-09 18:22:25 -0400

    added get route

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
-
-
-
-
 const app = express();
 
 
 app.use(express.static(__dirname));
+
+let messages = [
+  {name: 'Kool', message: 'you are fast'},
+  {name: 'Namari', message: 'Hi Daddy'}
+];
+
+app.get('/messages', (req, res) => {
+  res.send(messages)
+});
 
 
 
