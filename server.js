3a8094e1123586a420e953d60243a703d45f2357
Kool Havis <havisk@gmail.com>
2018-05-09 17:25:31 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+
+
+
+const app = express();
+
+
+app.use(express.static(__dirname));
+
+
+
+
+let server = app.listen(3000, () => {
+  console.log('server is listening on port ', server.address().port)
+});
